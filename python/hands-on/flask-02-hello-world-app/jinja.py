a38130fa74753748e0a3db795bbe6d112a289773
ikbol abulkasimov <ikbolabulkasimov@ikbols-MBP-2.fios-router.home>
2021-03-13 17:45:10 -0500

    calculator

--- python/hands-on/flask-02-hello-world-app/jinja.py
+++ python/hands-on/flask-02-hello-world-app/jinja.py
@@ -6,10 +6,17 @@ app = Flask(__name__)
 def head():
     return render_template('index.html', number1 = 12, number2 = 20)
 
-@app.route('/calculator')
-def number():
-    variable1, variable2 = 23, 45
-    return render_template('body.html', num1=variable1, num2=variable2, mult=variable1 * variable2)
+@app.route('/calculator/<int:number1>/<int:number2>/<string:operator>')
+def number(number1, number2, operator):
+    # variable1, variable2 = 23, 45
+    if operator == '+':
+        return render_template('body.html', num1=number1, num2=number2, mult=number1 + number2, opt=operator)
+    if operator == '-':
+        return render_template('body.html', num1=number1, num2=number2, mult=number1 - number2, opt=operator)
+    if operator == '*':
+        return render_template('body.html', num1=number1, num2=number2, mult=number1 * number2, opt=operator)
+    if operator == '÷':
+        return render_template('body.html', num1=number1, num2=number2, mult=number1 / number2, opt=operator)
 
 if __name__ == '__main__':
     app.run(debug=True)
